2d2a7165cb44add596fa1266d245661fa4c6af25
Calculus <51772836+calculus-dev@users.noreply.github.com>
2020-10-28 15:29:17 -0700

    Update tampermonkey.script.js

--- tampermonkey.script.js
+++ tampermonkey.script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Feat. Ghost System
-// @version      0.0.4
+// @version      0.0.5
 // @author       Calculus
 // @match        *://www.freeriderhd.com/*
 // @grant        unsafeWindow
@@ -8,54 +8,22 @@
 // @run-at       start
 // ==/UserScript==
 !function(){
-    function getFeaturedGhosts(callback = () => {}){
-        fetch(`https://raw.githubusercontent.com/Calculus0972/Official_Featured_Ghosts/master/ghosts.json`).then((response) => response.json()).then(json => {
-            callback(json);
+    fetch(`https://raw.githubusercontent.com/Calculus0972/Official_Featured_Ghosts/master/alias.json`).then((response) => response.json()).then(alias => {
+        fetch(`https://raw.githubusercontent.com/Calculus0972/Official_Featured_Ghosts/master/ghosts.json`).then((response) => response.json()).then(ghosts => {
+            for(var a in ghosts){
+                var b = [];
+                for(var c in ghosts[a]) for(var d in ghosts[a][c]) b.push(ghosts[a][c][d]);
+                for(c in b)
+                    if(parseInt(location.pathname.split("/t/")[1]) == parseInt(b[c].substring(10))) {
+                        var n = a;
+                        alias[a].forEach(e => {
+                            if(b[c].slice(-e.length) == e) n = e;
+                        });
+                        if(n.length > 15) n = n.substring(0, 12) + "...";
+                        d = document.getElementsByClassName('track-leaderboard-race');
+                        for(var e in d) if(d[e].innerText) if(d[e].innerText.toLowerCase() == n) d[e].style.color = "#e8a923";
+                    }
+            }
         });
-    }
-    function getFeaturedAlias(callback = () => {}){
-        fetch(`https://raw.githubusercontent.com/Calculus0972/Official_Featured_Ghosts/master/alias.json`).then((response) => response.json()).then(json => {
-            callback(json);
-        });
-    }
-    var alias = {};
-    getFeaturedAlias(aliases => {
-        for(const a in aliases){
-            alias[a] = {};
-            aliases[a].forEach((b, i = 0) => {
-                alias[a][b] = i++;
-            })
-        }
     });
-    getFeaturedGhosts(ghosts => {
-        var Arr = {};
-        for(const a in ghosts){
-            Arr[a] = [];
-            var arr = [ghosts[a].fast || [], ghosts[a].trick || [], ghosts[a].auto || [], ghosts[a].vehicle || []];
-            arr.forEach((e)=>{
-                e.forEach((e1)=>{
-                    Arr[a].push(e1);
-                });
-            });
-            for(const c in Arr[a])
-                if(parseInt(location.pathname.split("/t/")[1]) == parseInt(Arr[a][c].substring(10))) {
-                    var name = a;
-                    Object.keys(alias[a]).forEach(e => {
-                        if(Arr[a][c].slice(-e.length) == e){
-                            name = e
-                        }
-                    });
-                    if(name.length > 15){
-                        name = name.substring(0, 12) + "..."
-                    }
-                    for(const d in document.getElementsByClassName('track-leaderboard-race')){
-                        if(document.getElementsByClassName('track-leaderboard-race')[d].innerText) {
-                            if(document.getElementsByClassName('track-leaderboard-race')[d].innerText.toLowerCase() == name){
-                                document.querySelectorAll('.track-leaderboard-race')[d].style.color = "#e8a923"
-                            }
-                        }
-                    }
-                }
-        }
-    })
 }();
