b830b27499b3374a94c52202e08bbe39c16059ed
Calculus <51772836+calculus-dev@users.noreply.github.com>
2021-01-13 14:04:47 -0800

    Update tampermonkey.script.js

--- tampermonkey.script.js
+++ tampermonkey.script.js
@@ -1,27 +1,10 @@
 // ==UserScript==
 // @name         Feat. Ghost System
-// @version      0.0.5
+// @version      0.0.6
 // @author       Calculus
 // @match        *://www.freeriderhd.com/*
 // @match        *://frhd.kanoapps.com/*
 // ==/UserScript==
-!function(){
-    fetch(`https://raw.githubusercontent.com/Calculus0972/Official_Featured_Ghosts/master/alias.json`).then((response) => response.json()).then(alias => {
-        fetch(`https://raw.githubusercontent.com/Calculus0972/Official_Featured_Ghosts/master/ghosts.json`).then((response) => response.json()).then(ghosts => {
-            for(var a in ghosts){
-                var b = [];
-                for(var c in ghosts[a]) for(var d in ghosts[a][c]) b.push(ghosts[a][c][d]);
-                for(c in b)
-                    if(parseInt(location.pathname.split("/t/")[1]) == parseInt(b[c].substring(10))) {
-                        var n = a;
-                        alias[a].forEach(e => {
-                            if(b[c].slice(-e.length) == e) n = e;
-                        });
-                        if(n.length > 15) n = n.substring(0, 12) + "...";
-                        d = document.getElementsByClassName('track-leaderboard-race');
-                        for(var e in d) if(d[e].innerText) if(d[e].innerText.toLowerCase() == n) d[e].style.color = "#e8a923";
-                    }
-            }
-        });
-    });
-}();
+fetch("https://raw.githubusercontent.com/calculus-dev/Official_Featured_Ghosts/master/alias.json").then(r => r.json()).then(a => fetch("https://raw.githubusercontent.com/calculus-dev/Official_Featured_Ghosts/master/ghosts.json").then(r => r.json()).then((g, n) => {
+    for (const t in g) for (const e of g[t]) if (parseInt(location.pathname.split("/t/")[1]) == parseInt(e.url.slice(10))) for (const i of document.getElementsByClassName('track-leaderboard-race')) if (i.innerText && i.innerText.toLowerCase() == ((n = a[t] && Object.entries(a[t]).filter(([,b]) => b == e.url.slice(-b.length))?.[0]?.[1] || t) && n?.length > 15 && n.slice(0, 12) + "..." || n)) i.style.color = "#e8a923";
+}));
