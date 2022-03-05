65eafa8f5fa04c6b804d1e7b566e3e85aa558a20
Calculus <tahar.ayoob@gmail.com>
2022-03-04 16:09:54 -0800

    Minor improvements

--- display.js
+++ display.js
@@ -7,7 +7,7 @@
 // ==/UserScript==
 
 fetch("https://raw.githubusercontent.com/calculamatrise/frhd_featured_ghosts/master/alias.json").then(r => r.json()).then(function(alias) {
-    fetch("https://raw.githubusercontent.com/calculamatrise/frhd_featured_ghosts/master/ghosts.json").then(r => r.json()).then(function(players) {
+    fetch("https://raw.githubusercontent.com/calculamatrise/frhd_featured_ghosts/master/data.json").then(r => r.json()).then(function(players) {
         for (let player in players) {
             for (const ghost in players[player]) {
                 if (parseInt(location.pathname.split("/t/")[1]) !== parseInt(ghost.split("/t/")[1])) {
@@ -15,8 +15,20 @@ fetch("https://raw.githubusercontent.com/calculamatrise/frhd_featured_ghosts/mas
                 }
 
                 for (const element of document.getElementsByClassName("track-leaderboard-race")) {
-                    if (element.innerText && element.innerText.toLowerCase() == ((player = alias[player] && Object.entries(alias[player]).filter(([,b]) => b == ghost.slice(-b.length))?.[0]?.[1] || player) && player.length > 15 && player.slice(0, 12) + "..." || player)) {
-                        element.style.color = "#e8a923";
+                    if (element.innerText === void 0) {
+                        continue;
+                    }
+
+                    let name;
+                    if (element.innerText.toLowerCase() == ((name = alias[player] && Object.entries(alias[player]).filter(([,b]) => b == ghost.slice(-b.length))?.[0]?.[1] || player) && name.length > 15 && name.slice(0, 12) + "..." || name)) {
+                        let color = "rgba(232, 169, 35, 0.4)";
+                        switch(players[player][ghost]) {
+                            case "fast": color = "rgba(120, 200, 200, 0.4)"; break;
+                            case "vehicle": color = "rgba(240, 200, 80, 0.4)"; break;
+                            case "trick": color = "rgba(160, 240, 40, 0.4)"; break;
+                        }
+
+                        element.parentElement.parentElement.style.setProperty("background-color", color);
                     }
                 }
             }
