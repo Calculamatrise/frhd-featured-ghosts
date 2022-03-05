435c92f5485449abbc6babb63a1b2d0d8bcfcbe6
Calculus <tahar.ayoob@gmail.com>
2022-03-04 16:29:52 -0800

    Tidying scripts

--- display.js
+++ display.js
@@ -19,17 +19,23 @@ fetch("https://raw.githubusercontent.com/calculamatrise/frhd_featured_ghosts/mas
                         continue;
                     }
 
-                    let name;
-                    if (element.innerText.toLowerCase() == ((name = alias[player] && Object.entries(alias[player]).filter(([,b]) => b == ghost.slice(-b.length))?.[0]?.[1] || player) && name.length > 15 && name.slice(0, 12) + "..." || name)) {
-                        let color = "rgba(232, 169, 35, 0.4)";
-                        switch(players[player][ghost]) {
-                            case "fast": color = "rgba(120, 200, 200, 0.4)"; break;
-                            case "vehicle": color = "rgba(240, 200, 80, 0.4)"; break;
-                            case "trick": color = "rgba(160, 240, 40, 0.4)"; break;
-                        }
-
-                        element.parentElement.parentElement.style.setProperty("background-color", color);
+                    let name = alias[player] && Object.entries(alias[player]).filter(([,b]) => b == ghost.slice(-b.length))?.[0]?.[1] || player;
+                    if (name.length > 15) {
+                        name = name.slice(0, 12) + "...";
                     }
+
+                    if (element.innerText.toLowerCase() !== name) {
+                        return;
+                    }
+
+                    let color = "rgba(232, 169, 35, 0.4)";
+                    switch(players[player][ghost]) {
+                        case "fast": color = "rgba(120, 200, 200, 0.4)"; break;
+                        case "vehicle": color = "rgba(240, 200, 80, 0.4)"; break;
+                        case "trick": color = "rgba(160, 240, 40, 0.4)"; break;
+                    }
+
+                    element.parentElement.parentElement.style.setProperty("background-color", color);
                 }
             }
         }
